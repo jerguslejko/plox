465175aef63f2ab6ce1b24b20fbc5c2fb5cda10c
jerguslejko <jergus.lejko@gmail.com>
2020-04-10 17:21:29 +0200

    use proper import

--- lib/dot_printer.py
+++ lib/dot_printer.py
@@ -1,26 +1,26 @@
-from lib import expression as e
 from lib.token import Token, Type
+from lib.expression import BinaryExpression, UnaryExpression, LiteralExpression, GroupingExpression
 
 
 def print_expr(expr):
-    if isinstance(expr, e.BinaryExpression):
+    if isinstance(expr, BinaryExpression):
         return [
             '%s [label="BinaryExpression(%s)"]' % (id(expr), expr.operator.lexeme),
             '%s -> { %s, %s }' % (id(expr), id(expr.left), id(expr.right)),
         ] + print_expr(expr.left) + print_expr(expr.right)
 
-    if isinstance(expr, e.UnaryExpression):
+    if isinstance(expr, UnaryExpression):
         return [
             '%s [label="UnaryExpression(%s)"]' % (id(expr), expr.operator.lexeme),
             '%s -> %s' % (id(expr), id(expr.right)),
         ] + print_expr(expr.right)
 
-    if isinstance(expr, e.LiteralExpression):
+    if isinstance(expr, LiteralExpression):
         return [
             '%s [label="LiteralExpression(%s)"]' % (id(expr), expr.value),
         ]
 
-    if isinstance(expr, e.GroupingExpression):
+    if isinstance(expr, GroupingExpression):
         return [
             '%s [label="GroupingExpression"]' % id(expr),
             '%s -> %s' % (id(expr), id(expr.expression)),
