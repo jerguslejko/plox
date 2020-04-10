02c0ce8e4dd19a55fbdbcfaae7c118041ed63b14
jerguslejko <jergus.lejko@gmail.com>
2020-04-10 17:26:53 +0200

    expr helpers

--- lib/expression.py
+++ lib/expression.py
@@ -1,5 +1,14 @@
 class Expression:
-    pass
+    def show(self):
+        from lib.dot_printer import show_ast
+
+        show_ast(self)
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, vars(self))
+
+    def __eq__(self, other):
+        return isinstance(self, type(other)) and vars(self) == vars(other)
 
 
 class BinaryExpression(Expression):
