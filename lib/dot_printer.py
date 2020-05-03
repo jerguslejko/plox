72db664d4990f486545312c3a28fb3de4aab7aae
jerguslejko <jergus.lejko@gmail.com>
2020-05-04 00:21:13 +0200

    refactor dot printer

--- lib/dot_printer.py
+++ lib/dot_printer.py
@@ -1,247 +1,157 @@
-import os
-import tempfile
 from imgcat import imgcat
+from textwrap import indent
 from graphviz import Source
-from functools import reduce
+from lib import ast
 from lib.token import Token, Type
-from lib.ast import (
-    Block,
-    ExpressionStatement,
-    ReturnStatement,
-    IfStatement,
-    VariableDeclaration,
-    FunctionDeclaration,
-    PrintStatement,
-    WhileStatement,
-    BinaryExpression,
-    UnaryExpression,
-    LiteralExpression,
-    FunctionExpression,
-    LambdaExpression,
-    GroupingExpression,
-    TernaryExpression,
-    VariableExpression,
-    AssignmentExpression,
-    LogicalExpression,
-    CallExpression,
-)
 
 
-def print_expr(expr):
-    if isinstance(expr, LogicalExpression):
-        return (
-            [
-                '%s [label="LogicalExpression(%s)"]' % (id(expr), expr.token.lexeme),
-                "%s -> { %s, %s }" % (id(expr), id(expr.left), id(expr.right)),
-            ]
-            + print_expr(expr.left)
-            + print_expr(expr.right)
+def dot_node(node, label, children=[]):
+    values = []
+
+    values.append('%s [label="%s"]' % (id(node), label))
+
+    if isinstance(children, list):
+        for child in children:
+            if child is None:
+                continue
+
+            values.append(to_dot(child))
+            values.append(dot_transition(node, child))
+    elif isinstance(children, dict):
+        for label, child in children.items():
+            if child is None:
+                continue
+
+            if isinstance(child, list):
+                for (i, c) in enumerate(child):
+                    values.append(to_dot(c))
+                    values.append(dot_transition(node, c, "argument #%s" % i))
+            else:
+                values.append(dot_transition(node, child, label))
+                values.append(to_dot(child))
+    else:
+        values.append(to_dot(children))
+        values.append(dot_transition(node, children))
+
+    return "\n".join(values)
+
+
+def dot_transition(a, b, label=None):
+    if label:
+        return '%s -> %s [label="%s"]' % (id(a), id(b), label)
+
+    return "%s -> %s" % (id(a), id(b))
+
+
+def dot_block(name, body):
+    return "%s {\n%s\n}" % (name, indent(body, "\t"))
+
+
+def dot_digraph(node):
+    return dot_block("digraph", to_dot(node))
+
+
+def to_dot(node):
+    if node is None:
+        return ""
+
+    if isinstance(node, ast.Program):
+        return dot_node(node, "Program", node.statements)
+
+    if isinstance(node, ast.VariableDeclaration):
+        return dot_node(
+            node, "VariableDeclaration(%s)" % node.identifier.lexeme, node.initializer
         )
 
-    if isinstance(expr, BinaryExpression):
-        return (
-            [
-                '%s [label="BinaryExpression(%s)"]' % (id(expr), expr.operator.lexeme),
-                "%s -> { %s, %s }" % (id(expr), id(expr.left), id(expr.right)),
-            ]
-            + print_expr(expr.left)
-            + print_expr(expr.right)
+    if isinstance(node, ast.FunctionDeclaration):
+        return dot_node(
+            node,
+            "FunctionDeclaration(%s)(%s)"
+            % (node.name.lexeme, ", ".join([p.lexeme for p in node.parameters])),
+            node.body,
         )
 
-    if isinstance(expr, UnaryExpression):
-        return [
-            '%s [label="UnaryExpression(%s)"]' % (id(expr), expr.operator.lexeme),
-            "%s -> %s" % (id(expr), id(expr.right)),
-        ] + print_expr(expr.right)
+    if isinstance(node, ast.Block):
+        return dot_node(node, "Block", node.statements)
 
-    if isinstance(expr, LiteralExpression):
-        return [
-            '%s [label="LiteralExpression(%s)"]' % (id(expr), expr.value),
-        ]
-
-    if isinstance(expr, GroupingExpression):
-        return [
-            '%s [label="GroupingExpression"]' % id(expr),
-            "%s -> %s" % (id(expr), id(expr.expression)),
-        ] + print_expr(expr.expression)
-
-    if isinstance(expr, TernaryExpression):
-        return (
-            [
-                '%s [label="TernaryExpression"]' % id(expr),
-                "%s -> { %s, %s, %s }"
-                % (id(expr), id(expr.test), id(expr.then), id(expr.neht)),
-            ]
-            + print_expr(expr.test)
-            + print_expr(expr.then)
-            + print_expr(expr.neht)
+    if isinstance(node, ast.IfStatement):
+        return dot_node(
+            node,
+            "IfStatement",
+            {"condition": node.test, "then": node.then, "else": node.neht},
         )
 
-    if isinstance(expr, VariableExpression):
-        return [
-            '%s [label="VariableExpression(%s)"]' % (id(expr), expr.variable.lexeme),
-        ]
+    if isinstance(node, ast.WhileStatement):
+        return dot_node(node, "WhileStatement", node.body)
 
-    if isinstance(expr, FunctionExpression):
-        return [
-            '%s [label="FunctionExpression(%s)"]'
-            % (id(expr), ", ".join(map(lambda p: p.lexeme, expr.parameters)),),
-            "%s -> %s" % (id(expr), id(expr.body)),
-        ] + print_statement(expr.body)
+    if isinstance(node, ast.ReturnStatement):
+        return dot_node(node, "ReturnStatement", node.expression)
 
-    if isinstance(expr, LambdaExpression):
-        return [
-            '%s [label="LambdaExpression(%s)"]'
-            % (id(expr), ", ".join(map(lambda p: p.lexeme, expr.parameters)),),
-            "%s -> %s" % (id(expr), id(expr.expression)),
-        ] + print_expr(expr.expression)
+    if isinstance(node, ast.PrintStatement):
+        return dot_node(node, "PrintStatement", node.expressions)
 
-    if isinstance(expr, AssignmentExpression):
-        return [
-            '%s [label="AssignmentExpression(%s)"]' % (id(expr), expr.left.lexeme),
-            "%s -> %s" % (id(expr), id(expr.right)),
-        ] + print_expr(expr.right)
+    if isinstance(node, ast.ExpressionStatement):
+        return dot_node(node, "ExpressionStatement", node.expression)
 
-    if isinstance(expr, CallExpression):
-        return (
-            [
-                '%s [label="CallExpression"]' % id(expr),
-                "%s -> %s" % (id(expr), id(expr.callee)),
-                'subgraph cluster_%s_callee { label="callee" %s }'
-                % (id(expr), id(expr.callee)),
-                "%s -> { %s }"
-                % (
-                    id(expr),
-                    ", ".join(map(lambda e: str(id(e)), reversed(expr.arguments))),
-                ),
-            ]
-            + print_expr(expr.callee)
-            + reduce(
-                lambda xs, expr: print_expr(expr) + xs,
-                expr.arguments,
-                [
-                    'subgraph cluster_%s_arguments { label="arguments"; %s }'
-                    % (id(expr), "; ".join(map(lambda e: str(id(e)), expr.arguments))),
-                ],
-            )
+    if isinstance(node, ast.AssignmentExpression):
+        return dot_node(node, "AssignmentExpression(%s)" % node.left.lexeme, node.right)
+
+    if isinstance(node, ast.GroupingExpression):
+        return dot_node(node, "GroupingExpression", node.expression)
+
+    if isinstance(node, ast.LogicalExpression):
+        return dot_node(node, "LogicalExpression", [node.left, node.right])
+
+    if isinstance(node, ast.CallExpression):
+        return dot_node(
+            node,
+            "CallExpression",
+            {"callee": node.callee, "arguments": node.arguments},
         )
 
-    raise ValueError("Expression [%s] not supported" % type(expr))
+    if isinstance(node, ast.UnaryExpression):
+        return dot_node(node, "UnaryExpression(%s)" % node.operator.lexeme, node.right)
 
-
-def print_statement(statement):
-    if isinstance(statement, ExpressionStatement):
-        return [
-            '%s [label="ExpressionStatement"]' % id(statement),
-            "%s -> %s" % (id(statement), id(statement.expression)),
-        ] + print_expr(statement.expression)
-
-    if isinstance(statement, IfStatement):
-        return (
-            [
-                '%s [label="IfStatement"]' % id(statement),
-                "%s -> %s" % (id(statement), id(statement.test)),
-                "%s -> %s" % (id(statement), id(statement.then)),
-                (
-                    "%s -> %s" % (id(statement), id(statement.neht))
-                    if statement.neht
-                    else None
-                ),
-            ]
-            + print_expr(statement.test)
-            + print_statement(statement.then)
-            + (print_statement(statement.neht) if statement.neht else [])
+    if isinstance(node, ast.BinaryExpression):
+        return dot_node(
+            node,
+            "BinaryExpression(%s)" % node.operator.lexeme,
+            [node.left, node.right],
         )
 
-    if isinstance(statement, ReturnStatement):
-        return [
-            '%s [label="ReturnStatement"]' % id(statement),
-            "%s -> %s" % (id(statement), id(statement.expression)),
-        ] + print_expr(statement.expression)
+    if isinstance(node, ast.VariableExpression):
+        return dot_node(node, "VariableExpression(%s)" % node.variable.lexeme)
 
-    if isinstance(statement, PrintStatement):
-        return reduce(
-            lambda xs, expr: xs + print_expr(expr),
-            statement.expressions,
-            [
-                '%s [label="PrintStatement"]' % id(statement),
-                "%s -> { %s }"
-                % (
-                    id(statement),
-                    ", ".join(map(lambda e: str(id(e)), statement.expressions)),
-                ),
-            ],
+    if isinstance(node, ast.LiteralExpression):
+        return dot_node(node, "LiteralExpression(%s)" % node.value)
+
+    if isinstance(node, ast.FunctionExpression):
+        return dot_node(
+            node,
+            "FunctionExpression(%s)" % ", ".join([p.lexeme for p in node.parameters]),
+            node.body,
         )
 
-    if isinstance(statement, VariableDeclaration):
-        return [
-            '%s [label="VariableDeclaration(%s)"]'
-            % (id(statement), statement.identifier.lexeme),
-            "%s" % id(statement)
-            if statement.initializer is None
-            else "%s -> %s" % (id(statement), id(statement.initializer)),
-        ] + ([] if statement.initializer is None else print_expr(statement.initializer))
-
-    if isinstance(statement, Block):
-        return reduce(
-            lambda xs, statement: xs + print_statement(statement),
-            statement.statements,
-            [
-                '%s [label="Block"]' % id(statement),
-                "%s -> { %s }"
-                % (
-                    id(statement),
-                    ", ".join(map(lambda s: str(id(s)), statement.statements)),
-                ),
-            ],
-        ) + [
-            "subgraph cluster_%s_block { %s }"
-            % (
-                id(statement),
-                "; ".join(map(lambda e: str(id(e)), statement.statements)),
-            ),
-        ]
-
-    if isinstance(statement, WhileStatement):
-        return (
-            [
-                '%s [label="WhileStatement"]' % id(statement),
-                "%s -> { %s, %s }"
-                % (id(statement), id(statement.test), id(statement.body)),
-            ]
-            + print_expr(statement.test)
-            + print_statement(statement.body)
+    if isinstance(node, ast.LambdaExpression):
+        return dot_node(
+            node,
+            "LambdaExpression(%s)" % ", ".join([p.lexeme for p in node.parameters]),
+            node.expression,
         )
 
-    if isinstance(statement, FunctionDeclaration):
-        return [
-            '%s [label="FunctionDeclaration(%s)(%s)"]'
-            % (
-                id(statement),
-                statement.name.lexeme,
-                ", ".join(map(lambda p: p.lexeme, statement.parameters)),
-            ),
-            "%s -> %s" % (id(statement), id(statement.body)),
-        ] + print_statement(statement.body)
+    if isinstance(node, ast.TernaryExpression):
+        return dot_node(
+            node,
+            "TernaryExpression",
+            {"test": node.test, "then": node.then, "else": node.neht},
+        )
 
-    raise ValueError("Statement [%s] not supported" % type(statement))
+    raise ValueError("[dot printer] AST node [%s] not supported" % node.__class__)
 
 
-def print_program(ast):
-    return reduce(
-        lambda xs, statement: xs + print_statement(statement),
-        ast.statements,
-        [
-            '%s [label="Program"]' % id(ast),
-            "%s -> { %s }"
-            % (id(ast), ", ".join(map(lambda s: str(id(s)), ast.statements))),
-        ],
-    )
+def ast_to_dot(node):
+    return dot_digraph(node)
 
 
-def to_dot(ast):
-    return "digraph { %s }" % "; ".join(filter(lambda x: x, print_program(ast)))
-
-
-def show_ast(ast):
-    imgcat(Source(to_dot(ast), format="png").pipe())
+def ast_to_image(ast):
+    imgcat(Source(ast_to_dot(ast), format="png").pipe())
